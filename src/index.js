024798eeeb8707b5536a72994ccdb3ceed59c8ce
Angiegru <angiegrullon01@gmail.com>
2024-01-21 11:36:33 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 8089;
-// const Pets = re
 
 const publicDir = path.join(__dirname, '..', 'public');
 // here we are rendering the Static file 
@@ -15,21 +14,17 @@ app.use(staticAssets);
 
 app.use(express.json())
 
-const pets = [];
+//const pets = [];
 
 
-app.get('/test', (req, res) => {
-    console.log("hello?")
-    res.send("Hello world!").status(200);
+app.get('/', (req, res) => {
+    res.send("Hello World, this server is live").status(200);
 });
 
-// app.get('/getpets', async (req, res) => {
-//     res.send(pets).status(200);
-// });
 
 app.get('/getPets', async(req, res) => {
     const data = await petHelper.getAllPets();
-    console.log(data, "hello ? ");
+    console.log(data);
     res.send(data).status(200);
 });
 
@@ -38,9 +33,9 @@ app.get('/getPets', async(req, res) => {
 app.post('/addPets', async(req,res) => {
     console.log(req.body, "Testing addpets");
     try {
-        const {name, picture, species, isFriendly} = req.body;
+        const {pet_name, picture_url, species, is_friendly} = req.body;
         const currPet = {
-            name, picture, species, isFriendly
+            pet_name, picture_url, species, is_friendly
         }
         pets.push(currPet);
         res.send(currPet).status(200);
@@ -48,6 +43,15 @@ app.post('/addPets', async(req,res) => {
         res.send(err).status(404);
     }
 })
+
+app.delete('/deletepets:id', async (req, res) => {
+    const {id} = req.params;
+    const data = await petHelper.deletePets(id);
+    const foundPet = pets.findIndex(pet => Number(id) === pet.id);
+    pets.splice(foundPet, 1);
+    console.log(id)
+    res.send('deleted').status(200);
+});
 
 app.listen(PORT, () => {
     console.log('http://localhost:8089')
